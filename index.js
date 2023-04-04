bd517c308eca8f2fd8cf9a591b63dae7324fa7f2
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-04-04 16:40:15 -0300

    true aws configure git commit -m true

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const {preloadProduct} = require("./src/controllers/products");
 const {conn} = require("./src/db");
 
-conn.sync({force: false}).then(() => {
+conn.sync({force: true}).then(() => {
   preloadProduct();
   server.listen(3001);
   console.log("server is running on port 3001");
