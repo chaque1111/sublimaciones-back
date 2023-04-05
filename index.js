ac84b85035c22a9311cefb34de8b0e1f056d11df
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-04-05 15:13:57 -0300

    uwu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const {preloadProduct} = require("./src/controllers/products");
 const {conn} = require("./src/db");
 
-conn.sync({force: true}).then(() => {
+conn.sync({force: false}).then(() => {
   preloadProduct();
   server.listen(3001);
   console.log("server is running on port 3001");
